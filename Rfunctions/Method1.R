e431f3a8700b4cab692f18ddd8c5f7f43c340466
Corine Baayen <COBA@lundbeck.com>
2020-12-17 13:57:18 +0100

    Fix error method1 due to uniroot

--- Rfunctions/Method1.R
+++ Rfunctions/Method1.R
@@ -1,4 +1,5 @@
 #calculate critval c using method 1:
+require(BB)
 
 Method1 <- function(uk,  #upper bounds for all analyses up to and including current stage k
                     lk,  #lower bounds for all analyses up to and including current stage k
@@ -29,15 +30,23 @@ Method1 <- function(uk,  #upper bounds for all analyses up to and including curr
     }
   }
   
-  c <- uniroot(function(x){pmvnorm(lower = c(lk[0:(k-1)],uk[k],-Inf),
-                                   upper = c(uk[0:(k-1)],Inf,x),
-                                   mean=rep(0,k+1),
-                                   sigma= sigmaZk) - 
-      pmvnorm(lower = c(lk[0:(k-1)],-Inf,x),
-              upper = c(uk[0:(k-1)],lk[k],Inf),
-              mean=rep(0,k+1),
-              sigma= sigmaZk)},
-      lower = lk[k],
-      upper = uk[k]*1.1)$root
+  f <- function(x){
+    y <- pmvnorm(lower = c(lk[0:(k-1)],uk[k],-Inf),
+                           upper = c(uk[0:(k-1)],Inf,x),
+                           mean=rep(0,k+1),
+                           sigma= sigmaZk) - 
+         pmvnorm(lower = c(lk[0:(k-1)],-Inf,x),
+                           upper = c(uk[0:(k-1)],lk[k],Inf),
+                           mean=rep(0,k+1),
+                           sigma= sigmaZk)
+    as.numeric(y)
+    }
+  
+  c <- try(uniroot(f, lower = lk[k], upper = uk[k]*1.1)$root,silent=T)
+  if(inherits(c,"try-error")){
+    warning("uniroot produced an error, switching to dfsane")
+    c <- BB::dfsane(f, par = (uk[k]*1.1+lk[k])/2, control = list(maxit = 1000, tol = 1e-7), quiet = TRUE)$par
+  }
+  
   c
 }
